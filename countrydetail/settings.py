1509050b5cb4d234c6ab7932de5b57e0e0b5fddd
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-05-05 20:33:29 +0600

    added third party apps, static file configuration

--- countrydetail/settings.py
+++ countrydetail/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Third party
     'rest_framework',
+    'drf_yasg',
 
     # Internal
     'countries'
@@ -121,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # Optional if using custom static files
+STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
