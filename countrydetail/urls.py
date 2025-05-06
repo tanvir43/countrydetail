8d1a38d027924de762f7bda425dbe342dc2f9a05
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-05-06 19:23:19 +0600

    using JWTAuthentication for swagger

--- countrydetail/urls.py
+++ countrydetail/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 
 from rest_framework import permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 
 schema_view = get_schema_view(
@@ -17,16 +17,17 @@ schema_view = get_schema_view(
       default_version='v1',
       description="API documentation for the Country app"
    ),
-   public=False,
-   permission_classes=(permissions.IsAuthenticated,),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=(JWTAuthentication,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('countries.api.urls')),
-    path('', include('countries.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('api/', include('countries.api.urls')), # api interface
+    path('', include('countries.urls')),  # web interface
 
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
 
