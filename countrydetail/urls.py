e4ff264240412df2c954f56361a508d5ede09f06
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-05-06 13:04:52 +0600

    added swagger api doc permission layer, include web views

--- countrydetail/urls.py
+++ countrydetail/urls.py
@@ -1,27 +1,15 @@
-"""
-URL configuration for countrydetail project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView, LogoutView
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls.static import static
+
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -29,17 +17,20 @@ schema_view = get_schema_view(
       default_version='v1',
       description="API documentation for the Country app"
    ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
+   public=False,
+   permission_classes=(permissions.IsAuthenticated,),
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('countries.api.urls')),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include('countries.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
