5cfa16a1d9130e14e174fd7553351c4633d7ab8a
Tanvir Raihan <tanvirraihan@finder-lbs.com>
2025-05-06 18:26:13 +0600

    added access and refresh token api endpoint

--- countries/api/urls.py
+++ countries/api/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView as AccessTokenView,
+    TokenRefreshView as RefreshTokenView,
+)
+
 from .views import (
     CountryListCreateAPIView,
     CountryRetrieveUpdateDestroyAPIView,
@@ -7,10 +13,14 @@ from .views import (
     CountrySearchAPIView,
 )
 
+
 urlpatterns = [
+    path('token/', AccessTokenView.as_view(), name='access_token'),
+    path('token/refresh/', RefreshTokenView.as_view(), name='refresh_token'),
+
     path('countries/', CountryListCreateAPIView.as_view(), name='country-list'),
     path('countries/<int:pk>/', CountryRetrieveUpdateDestroyAPIView.as_view(), name='country-detail'),
     path('countries/<int:pk>/same-region/', SameRegionAPIView.as_view(), name='same-region-countries'),
     path('languages/<str:code>/countries/', CountriesByLanguageAPIView.as_view(), name='countries-by-language'),
-    path('countries/search/', CountrySearchAPIView.as_view(), name='country-search'),
+    path('countries/search/', CountrySearchAPIView.as_view(), name='country-search'),   
 ]
